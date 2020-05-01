7a88b64dbffa17b0bb823c56812a4299d3d8ce27
Swexbe <erik@erikjohnsson.se>
2020-05-01 20:59:51 +0200

    Fix css always showing scrollbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,19 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import * as serviceWorker from "./serviceWorker";
-import {
-    DigitProviders,
-    DigitToast,
-    DigitDialog,
-} from "@cthit/react-digit-components";
+import { DigitProviders, DigitDialog } from "@cthit/react-digit-components";
 import { Route } from "react-router";
 import LinkITHeaderView from "./common/linkitheader.view";
+import "./index.css";
 
 ReactDOM.render(
     <DigitProviders
         children={
-            <div>
-                <DigitToast />
+            <div className="main">
                 <DigitDialog />
                 <LinkITHeaderView
                     renderMain={() => <Route component={App} />}
