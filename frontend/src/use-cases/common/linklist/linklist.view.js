981f112a1612eeff76264a7fcfeddd889673fb98
Swexbe <erik@erikjohnsson.se>
2020-05-01 16:51:40 +0200

    Rename views

---
+++ frontend/src/use-cases/common/linklist/linklist.view.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import { DigitList } from "@cthit/react-digit-components";
+import LinkItem from "./linkitem.component";
+
+export const LinkList = props => {
+    const { getLinks } = props;
+    const [links, setLinks] = useState([]);
+    const onDelete = item => {
+        console.log(item);
+    };
+
+    useEffect(() => {
+        getLinks().then(links => setLinks(links.data));
+    }, []);
+    return (
+        <DigitList
+            title={`Your Shortcuts (${links.length}):`}
+            items={links.map(link => LinkItem(link, onDelete))}
+            multipleExpanded={false}
+            alignSelf={"auto"}
+            size={true}
+        />
+    );
+};
